b8927ed1bfcd7d5e822da30ee224c4e53bc373fc
Roba gh <rfalghanmi@gmail.com>
2023-02-08 09:56:52 +0300

    added quiz page

---
+++ iHero/Quiz.swift
@@ -0,0 +1,30 @@
+//
+//  Quiz.swift
+//  iHero
+//
+//  Created by roba on 08/02/2023.
+//
+
+import SwiftUI
+
+struct Quiz: View {
+    var body: some View {
+        NavigationView(){
+        ZStack{
+            Color("bgColor")
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        }
+        .ignoresSafeArea()
+       
+     
+            
+        }
+        
+    }
+}
+
+struct Quiz_Previews: PreviewProvider {
+    static var previews: some View {
+        Quiz()
+    }
+}
