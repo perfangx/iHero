4adadde052d630e244e059a8ef44e867f13f6305
RemazAlghamdi <Remaz_AGH@outlook.com>
2023-02-20 09:49:37 +0300

    add sos number and ack

--- iHero/referencePage.swift
+++ iHero/referencePage.swift
@@ -176,17 +176,28 @@ struct OtherDetailView: View {
                 .ignoresSafeArea()
             
             VStack {
-                Image(images)
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
+                switch titles {
+                case "Acknowledgment" :
+                    Acknowledgment()
+                case "SOS Numbers":
+                    SOSNumbers()
+                    
+                default:
+                    Text("No information available.")
+                }
+            
+                    //                Image(images)
+                    //                    .resizable()
+                    //                    .aspectRatio(contentMode: .fit)
+                    //
+                    //                Text(titles)
+                    //                    .font(.largeTitle)
+                    //                    .fontWeight(.bold)
+                    //                    .padding(.top, 20)
                 
-                Text(titles)
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
-                    .padding(.top, 20)
-           
             }
             .navigationBarTitle(titles)
+            .accentColor(.white)
         }
     }
 }
@@ -724,6 +735,37 @@ struct referencePage: View {
     var body: some View {
         ImageGrid(images: images , titles: titles , otherImages: otherImages , otherTitles: otherTitles)
     }
+}
+
+struct Acknowledgment: View {
+var body: some View {
+
+        VStack(alignment: .leading , spacing: 4) {
+            
+            Text("Words cannot express our gratitude to Paramedic Specialist Mr.Nawaf Alsayed for his time and effort in assisting us with his knowledge and providing the best resources for the content of this app, Additionally, this App would not have been possible without the generous support from the Apple Developer academy and all the mentors for their invaluable patience and feedback.")
+                .padding()
+            Text("We hope this app would be a great resource to learn about first aid and help in saving lives, and remember: Play like a Hero, turn into a Hero")
+                .padding()
+          
+        }
+        .foregroundColor(.white)
+    
+}
+}
+
+struct SOSNumbers: View {
+var body: some View {
+ScrollView{
+
+        VStack(alignment: .leading , spacing: 4) {
+            Spacer()
+            Text("SOS NUMBERS")
+                .padding()
+          Spacer()
+        }
+        .foregroundColor(.white)
+    }
+}
 }
 
 
